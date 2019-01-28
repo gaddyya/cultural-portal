be2e743daa919fca0dc87a712bb71befe7194a48
gaddyya <gaddyya@gmail.com>
2019-01-28 21:00:38 +0300

    my version of our portal

---
+++ precache-manifest.336fa471e6719ceabebfdd71db07c3b3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdbed3bfb1c27befcea1",
+    "url": "/codejam5/static/css/main.4b9cc0cb.chunk.css"
+  },
+  {
+    "revision": "fdbed3bfb1c27befcea1",
+    "url": "/codejam5/static/js/main.fdbed3bf.chunk.js"
+  },
+  {
+    "revision": "39eb32a2bf4c5d544424",
+    "url": "/codejam5/static/css/1.d2a27136.chunk.css"
+  },
+  {
+    "revision": "39eb32a2bf4c5d544424",
+    "url": "/codejam5/static/js/1.39eb32a2.chunk.js"
+  },
+  {
+    "revision": "5c88ac452e4d55a303c6",
+    "url": "/codejam5/static/js/runtime~main.5c88ac45.js"
+  },
+  {
+    "revision": "75af3aed42ed33b993072b5217316ec9",
+    "url": "/codejam5/static/media/bel-mir.75af3aed.jpg"
+  },
+  {
+    "revision": "05be004b21ff4f5e9ab47b4cf8edb0d1",
+    "url": "/codejam5/static/media/nefont.05be004b.ttf"
+  },
+  {
+    "revision": "89a2695186b71e7e692ac9062f0fbdfd",
+    "url": "/codejam5/static/media/gaddyya.89a26951.jpg"
+  },
+  {
+    "revision": "955bb514baf4d06ad595f9e725791acf",
+    "url": "/codejam5/static/media/zmichgen.955bb514.jpg"
+  },
+  {
+    "revision": "c26d5d2a83c5ccacfe6487c4662852a0",
+    "url": "/codejam5/static/media/zerovers.c26d5d2a.jpg"
+  },
+  {
+    "revision": "092a527a4c0bb70caba247c89d4ed9cf",
+    "url": "/codejam5/static/media/mdgh.092a527a.jpg"
+  },
+  {
+    "revision": "98641b00656a2e5f40c0ef2dd9fbbd84",
+    "url": "/codejam5/static/media/fontello.98641b00.woff"
+  },
+  {
+    "revision": "c4666684c815cf3540a7048cd03733b3",
+    "url": "/codejam5/static/media/renardby.c4666684.png"
+  },
+  {
+    "revision": "c5f14199136643d595a3014fe4287175",
+    "url": "/codejam5/index.html"
+  }
+];
